e2568e4b66a16c7af654a21965da8455d587bbf2
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-02-11 11:32:44 +0100

    data uit api gehaald en getoond op pagina

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ console.log(axios);
 
 async function displayCharacterNames() {
     console.log("hallo functie");
-    const response = await axios.get("https://anapioficeandfire.com/api");
+    const response = await axios.get("https://anapioficeandfire.com/api/characters");
 
-    console.log("Res:" + response.data.characters)
+    // console.log(response)
+    // console.log(response.data)
+    const characterNameList = document.getElementById("characterNameList");
+    // console.log(characterNameList)
+
+    for (const character of response.data) {
+        console.log(character);
+
+        const listItem = document.createElement("li");
+
+        const nameitem = () => {
+            if (character.name === "") {
+                return character.aliases;
+            } else {
+                return character.name;
+            }
+        };
+
+        listItem.textContent = nameitem() + " : " + character.gender + " aka: " + character.aliases;
+
+        console.log(listItem);
+
+        characterNameList.appendChild(listItem);
+    }
+
 }
+
 
 displayCharacterNames();
